6c914f9c787537d46c56c8beaf7dee90267c6110
Uttam Thakur <43903720+uttam-thakur@users.noreply.github.com>
2022-01-14 19:26:37 +0530

    initial commit

---
+++ scrip.js
@@ -0,0 +1,75 @@
+console.log("Welcome to Tic Tac Toe game")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "😀"
+let isgameover = false;
+
+// Function to change the turn
+const changeTurn = ()=>{
+    return turn === "😀"? "😈": "😀"
+}
+
+// Function to checking for a winning
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+        
+        }
+    })
+}
+
+//  Logic
+// music.play()
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+                // gameover.play()
+            } 
+            else{
+                music.play();
+                
+            }
+        }
+    })
+})
+
+// Add onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    music.pause()
+
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+
+    });
+    turn = "😀"; 
+    isgameover = false
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+    // music.play()
+})
+
